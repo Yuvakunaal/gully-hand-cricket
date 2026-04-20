9387a12caf060885013c5fa4e70e5ede2d99dc76
YuvaKunaal <bhavikunaal@gmail.com>
2026-04-20 16:16:37 +0530

    Lastest1

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png'],
+      manifest: {
+        name: 'GULLY HAND CRICKET',
+        short_name: 'Gully Cricket',
+        description: 'Play real-time multiplayer hand cricket with friends.',
+        theme_color: '#0a0e1a',
+        background_color: '#0a0e1a',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'favicon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
